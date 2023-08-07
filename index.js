173b484966b20ae30c70b2997578cc252cb38493
unknown <irfanhossain701@gmail.com>
2023-08-07 12:26:40 +0400

    express.static

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express=require('express')
 const app=express();
 
-app.use(express.text())  //which function will be used,it's called [Content-Type:text/plain]
+app.use(express.static(`${__dirname}/public/`))  //which function will be used,it's called
 
 app.get('/',(req,res)=>{
     res.send('This is home page')
 })
 
 app.post('/',(req,res)=>{
-
-    console.log(req.body)
     res.send('This is home page with post request')
 })
 
