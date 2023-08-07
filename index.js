d4fdaa8977e850b94e417ce513bde9b22f92790e
unknown <irfanhossain701@gmail.com>
2023-08-07 12:28:37 +0400

    express.static update

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express=require('express')
 const app=express();
 
-app.use(express.static(`${__dirname}/public/`))  //which function will be used,it's called
+app.use(express.static(`${__dirname}/public/`,{
+    index:'home.html'
+}))  //which function will be used,it's called
 
 app.get('/',(req,res)=>{
     res.send('This is home page')
