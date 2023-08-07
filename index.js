7a057e0a1cf2a5dd911daa9ea62dda88c8886f6b
unknown <irfanhossain701@gmail.com>
2023-08-07 13:52:20 +0400

    app.route

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
 const express = require('express')
 const app = express();
 
-//Methods
-
-//app.enable('case sensitive routing')
-//app.disable('case sensitive routing')
-app.all('/about', (req, res) => {
-    res.send("welcome to application home")
-})
-
-app.param('id',(req,res,next,id)=>{
-
-    const user={
-        name:"Irfan",
-        age:"28"
-    }
-    req.userDetails=user
-    next();
-})
-app.get('/user/:id', (req, res) => {
-    console.log(req.userDetails)
-    res.send("welcome to application home")
-})
-
-
-
-
+app.route('/about/mission')
+    .get((req,res)=>{
+        res.send("Get Request")
+    })
+    .post((req,res)=>{
+        res.send("Post Request")
+    })
+    .put((req,res)=>{
+        res.send("Put Request")
+    })
 app.listen(3000, () => {
     console.log('listening on port 3000')
 })
