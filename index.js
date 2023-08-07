39fc727492737b0c7017c1440036d5270bebc6fb
unknown <irfanhossain701@gmail.com>
2023-08-07 11:03:41 +0400

    Fresh File

---
+++ index.js
@@ -0,0 +1,14 @@
+const express=require('express')
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send('This is home page')
+})
+
+app.post('/',()=>{
+    res.send('This is home page with post request')
+})
+
+app.listen(3000,()=>{
+    console.log('listening on port 3000')
+})
