e43d9ee95eba6e6cf7ac60c09d76469fb7fb6b01
unknown <irfanhossain701@gmail.com>
2023-08-07 14:26:43 +0400

    Template Engine Setup And render

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const express = require('express')
 const app = express();
 
-app.route('/about/mission')
-    .get((req,res)=>{
-        res.send("Get Request")
-    })
-    .post((req,res)=>{
-        res.send("Post Request")
-    })
-    .put((req,res)=>{
-        res.send("Put Request")
-    })
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render("pages/about")
+})
 app.listen(3000, () => {
     console.log('listening on port 3000')
 })
