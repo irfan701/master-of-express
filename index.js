9e6648ec420bac4bcf78487db1b4570252bbcb63
unknown <irfanhossain701@gmail.com>
2023-08-07 11:41:54 +0400

    express.json

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express=require('express')
 const app=express();
 
+app.use(express.json())  //which function will be used,it's called [Content-Type:application/json]
+
 app.get('/',(req,res)=>{
     res.send('This is home page')
 })
 
 app.post('/',(req,res)=>{
+    console.log(typeof req.body) //[sending json data and receiving as an object with json]
+    console.log(req.body.name)
     res.send('This is home page with post request')
 })
 
