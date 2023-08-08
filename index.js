9560e7c725d92b46eee37cd2d13b6ec671975325
irfan701 <34702754+irfan701@users.noreply.github.com>
2023-08-09 01:09:15 +0400

    baseUrl

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express=require('express')
 const app=express();
 
-app.get('/',(req,res)=>{
+const adminRouter=express.Router()
+
+adminRouter.get('/dashboard',(req,res)=>{
+    console.log(req.baseUrl)
+    res.send('Admin Dashboard')
+})
+app.use('/admin',adminRouter)
+
+app.get('/user/:id',(req,res)=>{
+    console.log(req.baseUrl)
     res.send('This is home page')
 })
-
-app.post('/',()=>{
-    res.send('This is home page with post request')
-})
-
 app.listen(3000,()=>{
     console.log('listening on port 3000')
 })
