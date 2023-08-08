3313148f7897e948da9744dcb5f22717cff2637a
irfan701 <34702754+irfan701@users.noreply.github.com>
2023-08-09 01:29:14 +0400

    originalUrl,path,hostname,protocol,ip method

--- index.js
+++ index.js
@@ -4,13 +4,29 @@ const app=express();
 const adminRouter=express.Router()
 
 adminRouter.get('/dashboard',(req,res)=>{
-    console.log(req.baseUrl)
-    res.send('Admin Dashboard')
+    console.log(req.baseUrl)         // /admin
+    console.log(req.originalUrl)    //  /admin/dashboard
+    console.log(req.path)    //  /dashboard
+    console.log(req.hostname)    //  localhost
+    console.log(req.protocol)    //  http
+    console.log(req.ip)    //  ::1
+    console.log(req.method)    //  GET
+
+    res.send('Admin Dashboard')git a
+
 })
 app.use('/admin',adminRouter)
+//  http://localhost:3000/user/2?filter=shirt
 
 app.get('/user/:id',(req,res)=>{
     console.log(req.baseUrl)
+    console.log(req.originalUrl)   // /user/2?filter=shirt
+    console.log(req.path)   // /user/2 [query string will not come]
+
+    console.log(req.hostname)    //  localhost
+    console.log(req.protocol)    //  http
+    console.log(req.ip)    //  ::1
+    console.log(req.method)    //  GET
     res.send('This is home page')
 })
 app.listen(3000,()=>{
