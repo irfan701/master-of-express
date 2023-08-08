4a5cd0c603d70173d32c7ce0b0eb470d30227fd2
irfan701 <34702754+irfan701@users.noreply.github.com>
2023-08-09 02:20:47 +0400

    params,query,body

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
 const express=require('express')
 const app=express();
-
-const adminRouter=express.Router()
-
-adminRouter.get('/dashboard',(req,res)=>{
-    console.log(req.baseUrl)         // /admin
-    console.log(req.originalUrl)    //  /admin/dashboard
-    console.log(req.path)    //  /dashboard
-    console.log(req.hostname)    //  localhost
-    console.log(req.protocol)    //  http
-    console.log(req.ip)    //  ::1
-    console.log(req.method)    //  GET
-
-    res.send('Admin Dashboard')
-
-})
-app.use('/admin',adminRouter)
-//  http://localhost:3000/user/2?filter=shirt
+app.use(express.json())
 
 app.get('/user/:id',(req,res)=>{
-    console.log(req.baseUrl)
-    console.log(req.originalUrl)   // /user/2?filter=shirt
-    console.log(req.path)   // /user/2 [query string will not come]
 
-    console.log(req.hostname)    //  localhost
-    console.log(req.protocol)    //  http
-    console.log(req.ip)    //  ::1
-    console.log(req.method)    //  GET
-    res.send('This is home page')
+    console.log(req.params)   // { id: '2' } // [query string/parameter will not come]
+    console.log(req.query)   // { filter: 'shirt' } // [only query string/parameter come]
+    res.send("http://localhost:3000/user/2?filter=shirt")
+
+})
+
+app.post('/user',(req,res)=>{
+
+    console.log(req.body)   // { name: 'irfan' }
+    res.send("http://localhost:3000/user")
 })
 app.listen(3000,()=>{
     console.log('listening on port 3000')
