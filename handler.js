c9e494755a5733f8f0da0d1d9108b772445c3c67
irfan701 <34702754+irfan701@users.noreply.github.com>
2023-08-10 23:55:28 +0400

    req.accepts, req.get

--- handler.js
+++ handler.js
@@ -1,8 +1,10 @@
 const handler=(req,res)=>{
-    //console.log(req.secure) //false
-    console.log(req.app.get('view engine'))  //req er  moddey app er object/refernce bind kora asey ;tai externally amra bahir tekey app ke use kortey parvo
-    res.send("http://localhost:3000/user/2?filter=shirt")
 
+    if (req.accepts('html')){
+        res.render();
+    }else{
+        res.json({ user: 'geek' })
+    }
 }
 
 module.exports=handler;
--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const handler=require('./handler')
 
 app.get('/user',handler)
 
+app.get('/xyz',(req,res)=>{
+    console.log(req.get('accept'))
+    res.send(req.get('accept'))
+})
+
 app.listen(3000,()=>{
     console.log('listening on port 3000')
 })
