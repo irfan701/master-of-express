f1ba6a268647f0485575e3d625520f5318d27ef0
irfan701 <34702754+irfan701@users.noreply.github.com>
2023-08-10 22:33:08 +0400

    secure

--- index.js
+++ index.js
@@ -2,19 +2,13 @@ const express=require('express')
 const app=express();
 app.use(express.json())
 
-app.get('/user/:id',(req,res)=>{
+app.get('/user',(req,res)=>{
 
-    console.log(req.params)   // { id: '2' } // [query string/parameter will not come]
-    console.log(req.query)   // { filter: 'shirt' } // [only query string/parameter come]
+    console.log(req.secure)
     res.send("http://localhost:3000/user/2?filter=shirt")
 
 })
 
-app.post('/user',(req,res)=>{
-
-    console.log(req.body)   // { name: 'irfan' }
-    res.send("http://localhost:3000/user")
-})
 app.listen(3000,()=>{
     console.log('listening on port 3000')
 })
