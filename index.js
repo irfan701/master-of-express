e850a605bd9404e2a64fbddc3b60413b71da053f
irfan701 <34702754+irfan701@users.noreply.github.com>
2023-08-11 18:57:01 +0400

    render,locals,headerSent

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express=require('express')
-const app=express();
+const express = require('express')
+const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('This is home page')
+app.set('view engine', 'ejs')
+app.get('/about', (req, res) => {
+    console.log(res.headersSent)
+    res.render('pages/about', {name: "Bangladesh"})
+    console.log(res.headersSent)
 })
 
-app.post('/',()=>{
-    res.send('This is home page with post request')
-})
+//kono akta module jodi kono akta place tekey response patay de.
+// .r tokon check korar jonno headerssent use kortey pare
 
 app.listen(3000,()=>{
     console.log('listening on port 3000')
