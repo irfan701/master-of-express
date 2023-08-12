3ee09c488212cb3d262ec28b48ebb0884d54ed99
irfan701 <34702754+irfan701@users.noreply.github.com>
2023-08-12 14:21:59 +0400

    error handling middleware

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(express.json())  // built-in middleware
 
 const logger=(req,res,next)=>{
     console.log(`${new Date(Date.now()).toLocaleString()} - ${req.method} -${req.originalUrl} -${req.protocol} - ${req.hostname} - ${req.ip}` )
-    next()
+    //next()
+    throw new Error("This is an error")
 }
 adminRouter.use(logger)
 
@@ -26,6 +27,11 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
+const errorMiddleware=(err,req,res,next)=>{
+    console.log(err.message)
+    res.status(500).send('There was a server side error')
+}
+adminRouter.use(errorMiddleware)
 
 const PORT=process.env.PORT||3000
 app.listen(PORT, () => {
