1798f29b7d2d3ab0bdc9e98673b0c03ce1eff207
irfan701 <34702754+irfan701@users.noreply.github.com>
2023-08-12 14:07:13 +0400

    router level middleware

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express')
 const app = express();
+const adminRouter=express.Router()
+
 
 
 const logger=(req,res,next)=>{
     console.log(`${new Date(Date.now()).toLocaleString()} - ${req.method} -${req.originalUrl} -${req.protocol} - ${req.hostname} - ${req.ip}` )
-    res.end()
+    next()
 }
-app.use(logger)
-app.get('/', (req, res) => {
+adminRouter.use(logger)
 
+//made a subapp
+adminRouter.get('/dashboard',(req,res)=>{
+    res.send('Dashboard')
+})
+
+app.use('/admin',adminRouter)
+
+
+app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
