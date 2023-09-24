957e4a65acbd5412f9a97b1b3f8545d8fc7dd988
irfan701 <34702754+irfan701@users.noreply.github.com>
2023-09-25 01:59:59 +0400

    Error Handling Synchronous

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
 const express = require('express')
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send(a)
+app.get('/', (req, res,next) => {
+    for (let i=0;i<10;i++){
+        if (i===5){
+            next('there was an error')
+        }else{
+            res.write('a')
+        }
+    }
+   // res.send(a)
+    res.end()
+})
+
+app.use((req,res,next)=>{
+    //res.status(404).send('Requested url was not found !')
+    next('Requested url was not found !') //next er moddey msg means; error soho porer middleware e patano
 })
 
 const errorMiddleware=(err,req,res,next)=>{
-    console.log(err.message)
-    if (err.message){
-        res.status(500).send(err.message)
-    }else{
-        res.status(500).send('There was a server side error')
+   // console.log(err.message)
+    if (res.headersSent){
+        next('There was a problem !')
+    }else {
+        if (err.message) {
+            res.status(500).send(err.message)
+        } else {
+            res.status(500).send('There was a server side error')
+        }
     }
-
 }
 
 app.use(errorMiddleware)
+
+//invisible default error handler
+/*
+app.use((req,res,next)=>{
+  //handle error here
+})
+
+ */
+
+
 const PORT=process.env.PORT||3000
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}}`)
